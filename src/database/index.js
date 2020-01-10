330de1035c88906e909dd0f2a9e5a33b3534af04
Humberto Luksevicius <luks.develop@gmail.com>
2020-01-09 21:49:17 -0300

    feat: criando loader de models

---
+++ src/database/index.js
@@ -0,0 +1,33 @@
+import Sequelize from 'sequelize';
+
+// import mongoose from 'mongoose';
+import User from '../app/models/User';
+// import File from '../app/models/File';
+// import Appointment from '../app/models/Appointment';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+    // this.mongoinit();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+    // .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  // mongoinit() {
+  //   this.mongoconnection = mongoose.connect(
+  //     'mongodb://localhost:27017/gobarber',
+  //     { useNewUrlParser: true, useFindAndModify: true }
+  //   );
+  // }
+}
+
+export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'OK' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Humberto Luksevicius',
+    email: 'email@email.com.br',
+    password_hash: '12345678',
+  });
+  return res.json(user);
 });
 
 module.exports = routes;
