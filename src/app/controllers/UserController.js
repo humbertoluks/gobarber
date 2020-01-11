39af2dedf6c7f2fcfcf7a070725cca3e8ee8c9e2
Humberto Luksevicius <luks.develop@gmail.com>
2020-01-11 17:43:40 -0300

    feat: adiciona o Update do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,8 +19,48 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ message: 'OK' });
+    // const schema = Yup.object().shape({
+    //   name: Yup.string(),
+    //   email: Yup.string().email(),
+    //   oldpassword: Yup.string().min(6),
+    //   password: Yup.string()
+    //     .min(6)
+    //     .when('oldpassword', (oldpassword, field) =>
+    //       oldpassword ? field.required() : field
+    //     ),
+    //   confirmPassword: Yup.string().when('password', (password, field) =>
+    //     password ? field.required().oneOf([Yup.ref('password')]) : field
+    //   ),
+    // });
+
+    // if (!(await schema.isValid(req.body))) {
+    //   return res.status(400).json({ message: 'Validation fails' });
+    // }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
