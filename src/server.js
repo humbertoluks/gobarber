41eed3814ff6ff90e21298b09151f499bb5c63e4
Humberto Luksevicius <luks.develop@gmail.com>
2020-01-16 21:12:07 -0300

    chore: reconfigure nodemon and node for executing debug on port 9229

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import app from './app';
 
 app.listen(3333, () => {
-  // console.log('server listening port 3333');
+  const PORT = process.env.PORT || 3333;
+  console.log(`Server is running in http://localhost:${PORT}`);
 });
