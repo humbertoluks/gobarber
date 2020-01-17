8156d850b8263149e9e78b357db4dda33fff15d3
Humberto Luksevicius <luks.develop@gmail.com>
2020-01-17 18:10:31 -0300

    feat: add user appointment notification

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -12,7 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
-    // this.mongoinit();
+    this.mongoinit();
   }
 
   init() {
@@ -23,12 +23,16 @@ class Database {
       .map(model => model.associate && model.associate(this.connection.models));
   }
 
-  // mongoinit() {
-  //   this.mongoconnection = mongoose.connect(
-  //     'mongodb://localhost:27017/gobarber',
-  //     { useNewUrlParser: true, useFindAndModify: true }
-  //   );
-  // }
+  mongoinit() {
+    this.mongoconnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
+  }
 }
 
 export default new Database();
