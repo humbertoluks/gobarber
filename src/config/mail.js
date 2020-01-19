64f8111aee778b5ca05416a53b13d525c5977077
Humberto Luksevicius <luks.develop@gmail.com>
2020-01-19 15:51:38 -0300

    feat: nodemailer configuration

---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e6bb1724b895c7',
+    pass: 'fd00053faae712',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * mail servers
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Gmail
+ * Mailtrap(Dev) mailtrap.io
+ */
